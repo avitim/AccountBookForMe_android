e19e0eed93720f8bf65411f34c99022d0c0c252c
Taki <show13k.t@icloud.com>
2021-03-07 02:10:07 +0900

    Web API開発ディレクトリを別リポジトリに移動

--- server/AccountBookForMe/src/main/java/com/example/AccountBookForMe/repository/ExpenseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.AccountBookForMe.repository;
-
-import com.example.AccountBookForMe.entity.Expense;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExpenseRepository extends JpaRepository<Expense, Long> {
-}
--- server/AccountBookForMe/src/main/java/com/example/AccountBookForMe/repository/PaymentMethodRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.AccountBookForMe.repository;
-
-import com.example.AccountBookForMe.entity.PaymentMethod;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Long> {
-}
